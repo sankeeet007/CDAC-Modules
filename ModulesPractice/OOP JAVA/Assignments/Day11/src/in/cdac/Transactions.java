b5c9a68b10650da33534c6f6f0568b205c8ec00f
sankeeet007 <solankesanket007@gmail.com>
2026-04-02 00:19:06 +0530

    Day11 Complete

--- ModulesPractice/OOP JAVA/Assignments/Day11/src/in/cdac/Transactions.java
+++ ModulesPractice/OOP JAVA/Assignments/Day11/src/in/cdac/Transactions.java
@@ -7,6 +7,57 @@ public class Transactions {
 	private boolean txStatus;
 	private boolean txArrears;
 	
+	
+	public int getTxId() {
+		return txId;
+	}
+
+
+	public void setTxId(int txId) {
+		this.txId = txId;
+	}
+
+
+	public String getTxDate() {
+		return txDate;
+	}
+
+
+	public void setTxDate(String txDate) {
+		this.txDate = txDate;
+	}
+
+
+	public float getTxAmount() {
+		return txAmount;
+	}
+
+
+	public void setTxAmount(float txAmount) {
+		this.txAmount = txAmount;
+	}
+
+
+	public boolean isTxStatus() {
+		return txStatus;
+	}
+
+
+	public void setTxStatus(boolean txStatus) {
+		this.txStatus = txStatus;
+	}
+
+
+	public boolean isTxArrears() {
+		return txArrears;
+	}
+
+
+	public void setTxArrears(boolean txArrears) {
+		this.txArrears = txArrears;
+	}
+
+
 	public Transactions(int txId, String txDate, float txAmount, boolean txStatus, boolean txArrears) {
 		this.txId = txId;
 		this.txDate = txDate;
@@ -14,5 +65,11 @@ public class Transactions {
 		this.txStatus = txStatus;
 		this.txArrears = txArrears;
 	}
+	
+	public String toString() {
+        return "Transaction{id=" + txId + ", Date=" + txDate +
+               ", amount=" + txAmount + ", status=" + txStatus +
+               ", arrears=" + txArrears + "}";
+    }
 	
 }
