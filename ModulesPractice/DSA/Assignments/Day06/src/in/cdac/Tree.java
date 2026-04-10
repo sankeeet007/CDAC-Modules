5a05e1b62a2bf73363519e910d99febf9b25cc52
PGCP-AC <PGCP-AC@students.acts.in>
2026-04-10 21:07:29 +0530

    Day 06 completed

---
+++ ModulesPractice/DSA/Assignments/Day06/src/in/cdac/Tree.java
@@ -0,0 +1,12 @@
+package in.cdac;
+
+
+
+public interface Tree {
+	public void setRoot(int data);
+	public void addLeft(int data);
+	public void addRight(int data);
+	public void printUsingPreorder(BinaryTreeADT.TNode root);
+	public void printUsingInorder(BinaryTreeADT.TNode root);
+	public void printUsingPostorder(BinaryTreeADT.TNode root);
+}
